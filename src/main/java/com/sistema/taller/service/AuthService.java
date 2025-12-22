4059f2875762a1155085d1c3b08001f45e118ab8
jharedXDXD <jhared.rivera18@unach.mx>
2025-12-22 11:32:29 -0600

    Create AuthService.java

---
+++ src/main/java/com/sistema/taller/service/AuthService.java
@@ -0,0 +1,35 @@
+package com.sistema.taller.service;
+
+import com.sistema.taller.dto.AuthRequest;
+import com.sistema.taller.dto.AuthResponse;
+import com.sistema.taller.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final UsuarioRepository usuarioRepository;
+
+    public AuthResponse login(AuthRequest request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var usuario = usuarioRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        String token = jwtService.generateToken(usuario);
+
+        return new AuthResponse(token);
+    }
+}
