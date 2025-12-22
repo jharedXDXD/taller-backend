442c6e2e1f06624b09588cc01a68ea64f81e1b07
jharedXDXD <jhared.rivera18@unach.mx>
2025-12-22 15:37:51 -0600

    Update DataInitializer.java

--- src/main/java/com/sistema/taller/config/DataInitializer.java
+++ src/main/java/com/sistema/taller/config/DataInitializer.java
@@ -1,32 +1,33 @@
 package com.sistema.taller.config;
 
 import com.sistema.taller.entity.Usuario;
+import com.sistema.taller.entity.Rol;
 import com.sistema.taller.repository.UsuarioRepository;
-
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 @RequiredArgsConstructor
-public class DataInitializer {
+public class DataInitializer implements CommandLineRunner {
 
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
 
-    @PostConstruct
-    public void init() {
+    @Override
+    public void run(String... args) {
+
         if (usuarioRepository.count() == 0) {
             Usuario admin = new Usuario();
-            admin.setUsername("admin");
-            admin.setPassword(passwordEncoder.encode("admin"));
-            admin.setRol("ADMIN");
+            admin.setEmail("admin@taller.com");
+            admin.setPassword(passwordEncoder.encode("admin123"));
+            admin.setRol(Rol.ADMIN);
             admin.setActivo(true);
 
             usuarioRepository.save(admin);
         }
     }
 }
+
+
