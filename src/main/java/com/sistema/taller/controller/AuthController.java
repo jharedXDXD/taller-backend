145582912f1d925f908e1f637d2eac20403fa089
jharedXDXD <jhared.rivera18@unach.mx>
2025-12-22 11:29:13 -0600

    Update AuthController.java

--- src/main/java/com/sistema/taller/controller/AuthController.java
+++ src/main/java/com/sistema/taller/controller/AuthController.java
@@ -1,46 +1,21 @@
 package com.sistema.taller.controller;
 
-import com.sistema.taller.entity.Usuario;
-import com.sistema.taller.service.JwtService;
-import com.sistema.taller.service.UsuarioService;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import com.sistema.taller.dto.AuthRequest;
+import com.sistema.taller.dto.AuthResponse;
+import com.sistema.taller.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class AuthController {
 
-    private final UsuarioService usuarioService;
-    private final JwtService jwtService;
+    private final AuthService authService;
 
     @PostMapping("/login")
-    public LoginResponse login(@RequestBody LoginRequest request) {
-
-        Usuario usuario = usuarioService.autenticar(
-                request.getUsername(),
-                request.getPassword()
-        );
-
-        String token = jwtService.generarToken(
-                usuario.getUsername(),
-                usuario.getRol()
-        );
-
-        return new LoginResponse(token);
-    }
-
-    @Data
-    static class LoginRequest {
-        private String username;
-        private String password;
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class LoginResponse {
-        private String token;
+    public AuthResponse login(@RequestBody AuthRequest request) {
+        return authService.login(request);
     }
 }
